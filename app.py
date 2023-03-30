df5f7617a4465e26efc3bb21b764dc2ed2ef21fa
hexoramthehacker <114224280+hexoramthehacker@users.noreply.github.com>
2023-03-30 19:05:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class Todo(db.Model):
 from flask import Flask, render_template
 
 ...
-#Note change the base.html to the html or react file
+#Note change the base.html to the html or react file but check if the index.html is correct sha
 @app.route("/")
 def home():
     todo_list = Todo.query.all()
